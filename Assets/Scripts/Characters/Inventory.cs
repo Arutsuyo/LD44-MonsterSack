5e31dcf0ecc0dad942bf4e571fe664935f79e4dc
Nara <arutsuyo@gmail.com>
2019-04-26 20:51:14 -0700

    Initial

---
+++ Assets/Scripts/Characters/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [Header("Item Management")]
+    public List<BodyPart> Heads;
+    public List<BodyPart> Chest;
+    public List<BodyPart> Arms;
+    public List<BodyPart> Legs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

549a9590d0508273cd53f0d33d088c01da757507
Garrett <gbrown9816@gmail.com>
2019-04-27 13:30:30 -0700

    Script additions
    - Took kinematic off of player rigidbody.
    - updated player script.
    - Added a canvas with UI for HP and a Prompt. Made text for each text element.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
+    private int infectionRatio;
+    public double hp;
+    public Text hitpoints;
+    public Text infectRat;
+    public Text prompt;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        infectionRatio = 0;
+        hp = 100;
+        hitpoints.text = "HP: " + hp;
+        prompt.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+        hitpoints.text = "HP: " + hp;
+        replaceBodyPart();
     }
 
     void FixedUpdate()
@@ -35,6 +46,54 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
             rb.AddForce(movement * speed);
+        }
+    }
+
+    double HealthPoints()
+    {
+        return hp;
+    }
+    void replaceBodyPart()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            if (hp < 100)
+            {
+                prompt.text = "Limb repaired";
+                hp = hp + 25;
+                HealthPoints();
+            }
+            else
+                prompt.text = "MAX HEALTH";
+        }
+    }
+
+    void OnTriggerEnter(Collider other) //when a monster touches you depending on the color is the amount of health you lose.
+    {
+        if(other.tag == "Common Monster")
+        {
+            hp = hp - 10;
+            HealthPoints();
+        }
+        else if (other.tag == "Rare Monster")
+        {
+            hp = hp - 20;
+            HealthPoints();
+        }
+        else if (other.tag == "Legendary Monster")
+        {
+            hp = hp - 35;
+            HealthPoints();
+        }
+        else if (other.tag == "Epic Monster")
+        {
+            hp = hp - 50;
+            HealthPoints();
+        }
+        else if (other.tag == "Chroma Monster")
+        {
+            hp = hp - 75;
+            HealthPoints();
         }
     }
 }

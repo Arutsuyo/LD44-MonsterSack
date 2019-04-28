9a5709f7c0945bb922a1b56e4a4ded662580461f
Nara <arutsuyo@gmail.com>
2019-04-28 14:01:37 -0700

    Initial inventory display
    values must be tuned, and references made properly

--- Assets/Scripts/Items/UI/IconManager.cs
+++ Assets/Scripts/Items/UI/IconManager.cs
@@ -8,9 +8,24 @@ public class IconManager : MonoBehaviour
     [Header("Location Information")]
     public int startx, starty, gapx, gapy, width, height;
 
-    [Header("Image Prefab")]
-    public List<Image> imgs;
-
     [Header("Player Script References")]
     public Inventory inv;
+    public Player player;
+
+    private void OnGUI()
+    {
+        if (!player.showInv)
+            return;
+
+        for(int i = 0; i < inv.Sack.Count; i++)
+        {
+            Image img = inv.Sack[i].icon;
+            img.rectTransform.position = new Vector3(
+                startx + i * width + i * gapx,
+                starty + i * height + i * gapy,
+                0);
+            img.rectTransform.sizeDelta = new Vector2(width, height);
+            img.gameObject.SetActive(true);
+        }
+    }
 }

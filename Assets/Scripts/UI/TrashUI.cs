5f38e40f48a61653ee84ae74ce5f45e4654b2d9b
Chase Craig <ccraig@uoregon.edu>
2019-04-28 13:36:00 -0700

    Finished UI components

---
+++ Assets/Scripts/UI/TrashUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TrashUI : MonoBehaviour
+{
+    [Header("UI References")]
+    public Image Mask;
+    public Image Parent;
+    
+    [Header("Trash Size")]
+    public float CurSize;
+    public float MaxSize;
+    
+    [Header("Debugging")]
+    public bool CallUpdate = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CallUpdate)
+        {
+            CallUpdate = false;
+            UpdateUI();
+        }
+    }
+    public void UpdateUI()
+    {
+        Mask.rectTransform.sizeDelta = new Vector2(Mask.rectTransform.sizeDelta.x, Mathf.Lerp(0f, Parent.rectTransform.sizeDelta.y, CurSize/MaxSize));
+    }
+}

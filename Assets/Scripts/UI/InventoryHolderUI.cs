ef7c57888676e6c5255e56b1fe5882d29e471cd8
Chase Craig <ccraig@uoregon.edu>
2019-04-28 15:53:57 -0700

    Icons Finished, Prefabs Made

---
+++ Assets/Scripts/UI/InventoryHolderUI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InventoryHolderUI : MonoBehaviour
+{
+    //Purely holds the 6x4 array of inventory spaces..
+
+    public Image[] imageSlots = new Image[24];
+}

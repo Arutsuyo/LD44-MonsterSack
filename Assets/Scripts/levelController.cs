fa8980800df670cd03aad57a78a6f5300b869e35
tai <mantail0811@gmail.com>
2019-04-28 18:21:07 -0700

    add transport script and probably work, which means need test

---
+++ Assets/Scripts/levelController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class levelController : MonoBehaviour
+{
+
+    public string levelName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}

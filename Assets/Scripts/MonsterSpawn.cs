cdb451abb642b32d38b758af73b0f5816eff5e1a
Garrett <gbrown9816@gmail.com>
2019-04-28 17:36:08 -0700

    Created MonsterSpawn Script
    I have created a script that checks the availability of all spawn points within the level and then goes through the array of monster types and randomly picks one. Then instantiates said monster if the array of spawn points have one available.

---
+++ Assets/Scripts/MonsterSpawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawn : MonoBehaviour
+{
+    GameObject enemy;
+    public GameObject[] enemies;
+    public Transform[] spawns;
+
+    public float spawnTime = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawn()
+    {
+        if (availibilty() == true)
+        {
+            int spawnIndex = Random.Range(0, spawns.Length);
+            int enemyIndex = Random.Range(0, enemies.Length);
+            Instantiate(enemy, spawns[spawnIndex].position, spawns[spawnIndex].rotation);
+        }
+    }
+
+    bool availibilty()
+    {
+        for(int i = 0; i < 7; i++)
+        {
+            if(spawns[i] == enemies[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
